4953150ce21d8c3a85ae08d2237cdda792f6d651
Charles Gamble <cgamble@psg340b.com>
2018-01-30 11:24:54 -0600

    Initial Commit of Memory Script

---
+++ PowerShell/GetRemoteMemoryUtilization.ps1
@@ -0,0 +1,28 @@
+﻿function getMemUsers {
+    param($computer);
+
+    $table = @();
+
+    Get-Process |         
+        Group-Object -Property ProcessName |
+        %{
+            $process = $_;
+            $table += New-Object psobject -Property @{
+                Name       = $process.Name
+                WorkingSet = ($process.Group | Measure-Object WorkingSet -Sum).Sum / 1KB
+            };
+            
+            #Write-Host ("{0}`t{1}" -f $_.Name, ($_.Group | Measure-Object WorkingSet -Sum).Sum);
+        }
+
+        
+
+        
+        $table | Sort-Object -Property WorkingSet  -Descending |  
+        Format-Table Name, WorkingSet;
+}
+
+$computers = @();
+
+getMemUsers 127.0.0.1;
+
