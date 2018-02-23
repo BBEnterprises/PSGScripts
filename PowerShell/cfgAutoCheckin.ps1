3c15e8432cdef3b7276c6c37cf5ba4ec9286adf4
Charles Gamble <cgamble@psg340b.com>
2018-02-23 13:42:59 -0600

    Adding check-in logic

--- PowerShell/cfgAutoCheckin.ps1
+++ PowerShell/cfgAutoCheckin.ps1
@@ -4,30 +4,35 @@
     return (Compare-Object -ReferenceObject $(Get-Content $file1) -DifferenceObject $(Get-Content $file2));
 }
 
-function checkInFiles {
+function checkInChanges {
     $commitMessage = [datetime]::Now.ToString('MM/dd/yyyy') + ' Auto Commit';
 
-    git checkout staging;
-    git add ./;
-    git commit -m $commitMessage;
+    $status = git status 2>&1;
 
-    $output = git push 2>&1;
+    if ($status -match '') {    
+        git checkout staging;
+        git add ./;
+        git commit -m $commitMessage;
 
-    if ($output -match 'error:') {
-        alert ($output.Exception.Message -join "`n");
-    }
+        $output = git push 2>&1;
+
+        if ($output -match 'error:') {
+            alert ($output.Exception.Message -join "`n");
+        }
+    }   
 }
 
 function alert {
     param(
-        $message
+         [string]$message
+        ,[string]$subject
+
     );
 
     $dateTime   = [DateTime]::Now;
     $toMail     = 'cgamble@psg340b.com';
     #$toMail     = 'PSG340BTechOps@psg340b.com';
     $fromMail   = 'amonitor@psgconsults.com';
-    $subject    = '340b Cfg AutoCommit Failed!';
     $body       = $message;
     $smtp       = 'smtp.office365.com'
     $logMessage = '';
@@ -56,45 +61,24 @@ function alert {
 ############
 #Main Block#
 ############
-$localRepo   = 'C:\users\cgamble\Documents\code\PSG340B_EDISVC_Configs\cfgFiles\';
+$localRepo   = 'C:\Users\cgamble\Documents\Code\PSG340B_EDISVC_Configs\cfgFiles\';
 $regEx       = [System.Text.RegularExpressions.Regex]::Escape($localRepo) + '(.+)$';
 $alertLog    = 'C:\temp\autoCheckInAlertLog';
 $passFile    = 'C:\Temp\smtpPass';
 $checkInList = @();
-$localList   = @();
-$remoteList  = @();
-
-<#
-Build local list
-Build remote list
-Compare all files that exist in both, checkin changes
-look for any files missing from local list, add them, check in
-look for any files missing from remote list, remove them, check in 
-push
-
-#>
-
 
 Set-Location $localRepo;
 
 Get-ChildItem $localRepo -Recurse | ?{ -not $_.PSIsContainer }|%{
     $remotePath = '';
-
-
     if ($_.FullName -match $regEx) {
         $remotePath = '\\' + $matches[1];
-
-        
-        <#
         if (compareFiles $_.FullName $remotePath) {
             $checkInList += $remotePath;
             Copy-Item -Path $remotePath -Destination $_.FullName;
         }
-        #>
     }
 }
 
-if ($checkInList) {
-    checkInFiles;
-}
+checkInChanges;
 
