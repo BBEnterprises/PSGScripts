0c98db972b1501b83737e4238e892a9b11677068
Charles Gamble <cgamble@psg340b.com>
2018-02-27 10:26:38 -0600

    Implemented setSvc action
    1 - Created the setSvc function so that we can change the status and
    startup type of services during a deploy
    
    2 - Made the column names generic to support actions that have nothing
    to do with files. They are not 'col1', 'col2', etc. instead of
    'sourceFile' or 'destFile'.

--- DeployScripts/EDIMultithreading/EDIDEploy.ps1
+++ DeployScripts/EDIMultithreading/EDIDEploy.ps1
@@ -5,16 +5,16 @@
     );
 
     Get-Content -Path $cfgFile | ?{ $_ -notmatch '^\s*#'} | %{
-        ($action, $sourceFile, $destFile) = $_ -split '\s*:\s*';
+        ($action, $col1, $col2) = $_ -split '\s*:\s*';
         
         $actionList.Value += New-Object -TypeName psobject -Property @{
-            'sourceFile' = $sourceFile;
-            'destFile'   = $destFile;
             'actionType' = $action;
+            'col1'       = $col1;
+            'col2'       = $col2;
+            'col3'       = $col3;
+            'col4'       = $col4;
         }
     }
-
-    return 1;
 }
 
 function confirmCopies {
@@ -23,8 +23,13 @@ function confirmCopies {
     #First make sure all your deploy files exist
     $missingFile = 0
     foreach ($action in $actionList.Value) {   
-        if ( -not (Test-Path $action.sourceFile) ) {
-            Write-Host ("Missing File: {0}" -f $action.sourceFile);
+        #Continue if it's a setSvc since there isn't a file to verify
+        if ($action.actionType -eq 'setSvc') {
+            continue;
+        }
+
+        if ( -not (Test-Path $action.col1) ) {
+            Write-Host ("Missing File: {0}" -f $action.col1);
             $missingFile = 1;
         }
     }
@@ -35,15 +40,13 @@ function confirmCopies {
     $caption  = 'Review File Actions';
     $message  = "Carefully review the  actions listed in your terminal and confirm their accuracy`n";
     
-    $actionList.Value | select action, sourceFile, destFile | ft -AutoSize
+    $actionList.Value | select action, col1, col2, col3, col4 | ft -AutoSize
     
     $options  = [System.Management.Automation.Host.ChoiceDescription[]] @('Proceed', 'Halt');
 
     if ( $host.UI.PromptForChoice($caption, $message, $options, 1) ) {
         exit
     }
-
-    return 1;
 }
 
 function runActions {
@@ -53,13 +56,12 @@ function runActions {
         'copy'   = $function:copyFile;
         'rename' = $function:renameFile;
         'delete' = $function:deleteFile;
+        'setSvc' = $function:setSvc;
     }
 
     foreach ($action in $actionList.Value) {
         & $actionTypes.($action.actionType) $action;
     }
-
-    return 1;
 }
 
 function copyFile {
@@ -67,48 +69,50 @@ function copyFile {
     
 
     try {
-        Copy-Item -Path $action.sourceFile -Destination $action.destFile -ErrorAction Stop
-        Write-Host ("Copying: {0} to {1}" -f $action.sourceFile, $action.destFile);
+        Copy-Item -Path $action.col1 -Destination $action.col2 -ErrorAction Stop
+        Write-Host ("Copying: {0} to {1}" -f $action.col1, $action.col2);
     }
     catch {
-        Write-Host ("COPY ERROR :{0}, {1}" -f $action.sourceFile, $_.Exception.Message);
+        Write-Host ("COPY ERROR :{0}, {1}" -f $action.col1, $_.Exception.Message);
     }
-
-    return 1;
 }
 
 function renameFile {
     param($action);
 
     try {
-        Rename-Item -Path $action.sourceFile -NewName $action.destFile -ErrorAction Stop;
-        Write-Host ("Renaming: {0} to {1}" -f $action.sourceFile, $action.destFile);
+        Rename-Item -Path $action.col1 -NewName $action.col2 -ErrorAction Stop;
+        Write-Host ("Renaming: {0} to {1}" -f $action.col1, $action.col2);
     }
     catch {
-        Write-Host ( "RENAME ERROR: {0} -> {1}" -f $action.sourceFile, $action.destFile);
+        Write-Host ( "RENAME ERROR: {0} -> {1}" -f $action.col1, $action.col2);
     }
-
-    return 1;
 }
 
 function deleteFile {
     param($action);
 
     try {
-        Remove-Item -Path $action.sourceFile -ErrorAction Stop;
-        Write-Host ("Deleting: {0}" -f $action.sourceFile);
+        Remove-Item -Path $action.col1 -ErrorAction Stop;
+        Write-Host ("Deleting: {0}" -f $action.col1);
     }
     catch {
-        Write-Host ("DELETING ERROR: {0}" -f $action.sourceFile);
+        Write-Host ("DELETING ERROR: {0}" -f $action.col1);
     }
+}
 
-    return 1;
+function setSvc {
+    param($action);
+
+    Get-Service -ComputerName $action.col1 -Name $action.col2 | %{
+        $_ | Set-Service -Status $action.col3 -StartupType $action.col4;
+    }
 }
 
 ############
 #Main Block#
 ############
-$cfgFile  = 'C:\Users\cgamble\Documents\Code\PSGScripts\DeployScripts\22557\deploy.cfg';
+$cfgFile  = 'C:\Users\cgamble\Documents\Code\PSGScripts\DeployScripts\22759\deploy.cfg';
 #$cfgFile  = 'C:\users\cgamble\Documents\Code\PSGScripts\DeployScripts\TestDeploy\rollback.cfg';
 $actionList = @();
 
@@ -116,14 +120,7 @@ readCfg       $cfgFile ([ref]$actionList);
 confirmCopies ([ref]$actionList);
 runActions    ([ref]$actionList);
 
-
-
 <#
 TO DO:
-1 - Rename and delete functionality
-	- Adjust config to have a 'deploy action' column
-	- First three actions will be 'rename', 'delete', and 'copy'
 
-2 - Error handling on Copy-Item
-	- Wrap Copy-Item in Try-Catch, log any failures
 #>
